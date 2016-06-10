493a7711926d8ae0f2442b536170462541c4691c
Nikhil Surti <nikhil.surati@gmail.com>
2016-06-09 22:08:42 -0700

    Added getDimentionOfImage method to the width and height of image image upload

--- android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
+++ android/src/main/java/com/evollu/react/fcm/FIRMessagingModule.java
@@ -1,34 +1,59 @@
 package com.evollu.react.fcm;
 
+
+import com.facebook.react.bridge.LifecycleEventListener;
+
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
-import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.RemoteMessage;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import android.os.Bundle;
-import android.util.Log;
-
-import android.content.Context;
-
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
+
+
+
+
+
+
+
+
+
 public class FIRMessagingModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
     private final static String TAG = FIRMessagingModule.class.getCanonicalName();
+    private StorageReference mStorageRef;
+    private Uri mDownloadUrl = null;
+    private Uri mFileUri = null;
     Intent mIntent;
-
+    
     public FIRMessagingModule(ReactApplicationContext reactContext, Intent intent) {
         super(reactContext);
 
@@ -81,6 +106,59 @@ public class FIRMessagingModule extends ReactContextBaseJavaModule implements Li
     public void unsubscribeFromTopic(String topic){
         FirebaseMessaging.getInstance().unsubscribeFromTopic(topic);
     }
+    
+     @ReactMethod
+    public void getDimentionOfImage(String localFile,Promise promise) throws IOException {
+        Uri fileUri = Uri.parse(localFile);
+        Bitmap bitmap =  getBitmapFromUri(fileUri);
+        WritableMap map = Arguments.createMap();
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        map.putInt("width",width);
+        map.putInt("height",height);
+        promise.resolve(map);
+    }
+
+    @ReactMethod
+    public void UploadFileToFirebase(String localFile, String contentType, String bucket, String key, final Promise promise) throws IOException {
+
+        Uri fileUri = Uri.parse(localFile);
+        Bitmap bitmap =  getBitmapFromUri(fileUri);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 70, baos);
+        byte[] data = baos.toByteArray();
+
+            mStorageRef = FirebaseStorage.getInstance().getReferenceFromUrl(bucket);
+            StorageReference photoRef  = mStorageRef.child(key);
+            UploadTask uploadTask = photoRef.putBytes(data);
+            // Observe state change events such as progress, pause, and resume
+            uploadTask.addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+
+            @Override
+            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                float progress = (taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                sendEvent("FirebaseUploadProgressChanged", progress);
+                System.out.println("Upload is " + progress + "% done");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                promise.reject(exception.toString());
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                promise.resolve(downloadUrl.toString());
+            }
+        });
+
+
+//
+   }
+   // [END upload_from_uri]
+
 
     private void sendEvent(String eventName, Object params) {
     getReactApplicationContext()
