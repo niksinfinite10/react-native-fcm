83350451311b4aac68b96c2aa7c893dc6029894e
Nikhil Surti <nikhil.surati@gmail.com>
2016-06-09 22:22:44 -0700

    added image upload function

--- ios/RNFIRMesssaging.m
+++ ios/RNFIRMesssaging.m
@@ -4,6 +4,8 @@
 #import "RCTConvert.h"
 #import "RCTEventDispatcher.h"
 #import "RCTUtils.h"
+@import Photos;
+@import FirebaseMessaging;
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_8_0
 
@@ -14,6 +16,7 @@
 #define UIUserNotificationType      UIRemoteNotificationType
 
 #endif
+
 
 NSString *const FCMNotificationReceived = @"FCMNotificationReceived";
 
@@ -106,6 +109,57 @@ RCT_EXPORT_METHOD(requestPermissions)
         [app registerForRemoteNotificationTypes:(NSUInteger)types];
     }
 }
+
+RCT_REMAP_METHOD(UploadFileToFirebase,
+                 : (NSString*) localFile
+                 : (NSString*) contentType
+                 : (NSString*) bucket
+                 : (NSString*) key
+                 resolver:(RCTPromiseResolveBlock)resolve
+                 rejecter:(RCTPromiseRejectBlock)reject){
+  NSURL *url = [[NSURL alloc] initWithString:localFile];
+  PHFetchResult* assets = [PHAsset fetchAssetsWithALAssetURLs:@[url] options:nil];
+  PHAsset *asset = [assets firstObject];
+  [asset requestContentEditingInputWithOptions:nil
+                             completionHandler:^(PHContentEditingInput *contentEditingInput,
+                                                 NSDictionary *info) {
+                               NSURL *imageFile = contentEditingInput.fullSizeImageURL;
+                               
+
+  FIRStorage *storage = [FIRStorage storage];
+  // Create a storage reference from our storage service
+  FIRStorageReference *storageRef = [storage referenceForURL:bucket];
+
+  
+  // Create a reference to the file you want to upload
+  //NSString *storageLocation = [@"images/" stringByAppendingString:key];
+  FIRStorageReference *riversRef = [storageRef child:key];
+  
+  // Upload the file to the path "images/rivers.jpg"
+  FIRStorageUploadTask *uploadTask = [riversRef putFile:imageFile metadata:nil completion:^(FIRStorageMetadata *metadata, NSError *error) {
+    if (error != nil) {
+      // Uh-oh, an error occurred!
+      NSLog(@"Error in Uploading File to Firebase", error);
+      reject(@"Error", @"Failed upload file to firebase", error);
+    } else {
+      // Metadata contains file metadata such as size, content-type, and download URL.
+      NSURL *downloadURL = metadata.downloadURL;
+      NSLog(@"Successfully uploaded File to Firebase", metadata);
+      resolve(@ {@"downloadURL": downloadURL});
+    }
+  }];
+  
+  FIRStorageHandle observer = [uploadTask observeStatus:FIRStorageTaskStatusProgress
+                                                handler:^(FIRStorageTaskSnapshot *snapshot) {
+                                                  
+                                                  float progress = (float) snapshot.progress.totalUnitCount / snapshot.progress.completedUnitCount;
+                                                  [self.bridge.eventDispatcher sendAppEventWithName:@"FirebaseUploadProgressChanged" body: @{ @"progress": [NSNumber numberWithFloat:progress], @"key": key}];
+                                                  
+                                                }];
+                             }];
+  
+}
+
 
 RCT_EXPORT_METHOD(subscribeToTopic: (NSString*) topic)
 {
